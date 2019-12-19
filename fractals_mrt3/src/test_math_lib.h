5759de4334ea01ab84c187a9cc2ebc4f19f0c613
Laergner <gaertnerleonard@web.de>
2019-12-19 18:08:24 +0100

    added library for mathematical functions

---
+++ fractals_mrt3/src/test_math_lib.h
@@ -0,0 +1,13 @@
+/*
+ * test_math_lib.h
+ *
+ *  Created on: 19.12.2019
+ *      Author: gaert
+ */
+
+#ifndef SRC_TEST_MATH_LIB_H_
+#define SRC_TEST_MATH_LIB_H_
+
+int *test_get_iter();
+
+#endif /* SRC_TEST_MATH_LIB_H_ */
