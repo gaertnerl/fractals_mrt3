2feda8b2bbfb61e31c1b3c2215bd307b6234e7b1
gaertnerl <gaertnerleonard@web.de>
2019-12-31 15:02:31 +0100

    main now displays julia and mandelbrot sequentially

--- fractals_mrt3/src/main.c
+++ fractals_mrt3/src/main.c
@@ -3,10 +3,14 @@
 #include <stdio.h>
 
 int main(){
+
+
+
     complex_t julia_c = {0.4, 0.4};
     complex_t mandelbrot_z = {0,0};
 
-    printf("start...\n");
+    parameters.fraktal_t = JULIA;
+    printf("start Julia...\n");
     grafik_init_window();
     grafik_create_paint_area(
         parameters.xmin,
@@ -16,15 +20,28 @@ int main(){
         parameters.xpoints,
         parameters.ypoints
     );
-
     printf("drawing points... \n");
-    if(parameters.fraktal_t == JULIA) draw_all_points(julia_c);
-    if(parameters.fraktal_t == MANDELBROT) draw_all_points(mandelbrot_z);
-    printf("finished... \n");
-
+    draw_all_points(julia_c);
+    printf("finished julia ... wait for input \n");
     int t;
     scanf("%d", &t);
-    printf("the end ...\n");
+    grafik_close_window();
+
+    parameters.fraktal_t = MANDELBROT;
+    printf("start Mandelbrot...\n");
+    grafik_init_window();
+    grafik_create_paint_area(
+        parameters.xmin,
+        parameters.xmax,
+        parameters.ymin,
+        parameters.ymax,
+        parameters.xpoints,
+        parameters.ypoints
+    );
+    printf("drawing points... \n");
+    draw_all_points(mandelbrot_z);
+    printf("finished Mandelbrot ... wait for input \n");
+    scanf("%d", &t);
     grafik_close_window();
 
     return 0;
