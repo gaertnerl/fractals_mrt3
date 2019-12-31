2b38e7577caefa9cc4d26926e569d6f57b8bf42e
gaertnerl <gaertnerleonard@web.de>
2019-12-31 14:11:42 +0100

    main for testing

--- fractals_mrt3/src/main.c
+++ fractals_mrt3/src/main.c
@@ -1,9 +1,31 @@
-#include "test_math_lib.h"
-
+#include "fraktal.h"
+#include "graphic.h"
 #include <stdio.h>
 
 int main(){
-    int *arr_p = test_get_iter();
+    complex_t c = {1, 1};
+
+    printf("start...\n");
+    grafik_init_window();
+    grafik_create_paint_area(
+        parameters.xmin,
+        parameters.xmax,
+        parameters.ymin,
+        parameters.ymax,
+        parameters.xpoints,
+        parameters.ypoints
+    );
+
+    printf("drawing points... \n");
+    draw_all_points(c);
+    printf("finished... \n");
+
+    int t;
+    scanf("%d", &t);
+    printf("the end ...\n");
+    grafik_close_window();
+
+    return 0;
 };
 
 
